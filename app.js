a717707758c0c8041be29524eb5fe11d9de3eccb
Rashmi Ranjan Behera <110801220+Rashmi7205@users.noreply.github.com>
2023-10-08 10:33:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-    origin:"https://tiny-pudding-f3ef0e.netlify.app",
+    origin:"http://localhost:3000",
     credentials:true
 }));
 
