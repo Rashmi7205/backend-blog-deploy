0df8553b71b4a1b60b64074192fdf3ab2ca1fe27
Rashmi Ranjan Behera <110801220+Rashmi7205@users.noreply.github.com>
2023-10-08 12:22:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-import  express  from "express";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
-import blogRoutes from './routes/blogs.routes.js'
-import errorMiddleWare from './middleware/error.middleware.js';
-
+import blogRoutes from './routes/blogs.routes.js';
+import errorMiddleware from './middleware/error.middleware.js';
 
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+// Specify the origin of your frontend
+const corsOptions = {
+  origin: 'https://justwriteblog.netlify.app',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-})
+app.use(cors(corsOptions));
 
-app.use('/api/v1/user',userRoutes);
-app.use('/api/v1/blog',blogRoutes);
-
-app.all('*',(req,res)=>{
-    return res.status(400).send('OOPS!! PAGE NOT FOUND');
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/blog', blogRoutes);
 
+app.all('*', (req, res) => {
+  return res.status(404).send('OOPS!! PAGE NOT FOUND');
+});
 
-app.use(errorMiddleWare);
+app.use(errorMiddleware);
 
 export default app;
